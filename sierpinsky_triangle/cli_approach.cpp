af39be8bd64abff4aa84a5e903a767dc77dab896
Dmytro Kyrychenko <dm.kirichen@gmail.com>
2023-09-12 02:17:25 +0300

    feat: added working sierpinsky triangle generation in cli

--- sierpinsky_triangle/cli_approach.cpp
+++ sierpinsky_triangle/cli_approach.cpp
@@ -4,80 +4,63 @@
 
 using namespace std;
 
-/*
-
- *
- * *
- *   *
- * * * *
- *       *
- * *     * *
- *   *   *   *
- * * * * * * * *
- *               *
- * *             * *
- *   *           *   *
- * * * *         * * * *
- *       *       *       *
- * *     * *     * *     * *
- *   *   *   *   *   *   *   *
- * * * * * * * * * * * * * * * *
-
-*/
-
-void writeSubmatrixIntoMatrix(vector<vector<char>>& matrix, const vector<vector<char>>& submatrix, int posY, int posX) {
+void writeSubmatrixIntoMatrix(vector<vector<char>> &matrix, const vector<vector<char>> &submatrix, int posY, int posX)
+{
     // Assuming that submatrix size is less or equal to matrix size
-    for (int i = 0; i < submatrix.size(); i++) {
-        for (int j = 0; j < submatrix[i].size(); j++) {
-            matrix[i+posY][j+posX] = submatrix[i][j];
+    for (int i = 0; i < submatrix.size(); i++)
+    {
+        for (int j = 0; j < submatrix[i].size(); j++)
+        {
+            matrix[i + posY][j + posX] = submatrix[i][j];
         }
     }
 }
 
-vector<vector<char>> generateSierpinskyTriangle(int scaleOrder) {
-    if (scaleOrder == 0) {
+vector<vector<char>> generateSierpinskyTriangle(int scaleOrder)
+{
+    if (scaleOrder == 0)
+    {
         // Returning basic triangle
         vector<vector<char>> triangle = {{'*', ' '},
                                          {'*', '*'}};
         return triangle;
     }
+    else
+    {
+        int width = pow(2, scaleOrder + 1);
+        vector<vector<char>> megaMatrix;
+        megaMatrix.resize(width, vector<char>(width, ' ')); // setting size of matrix to scaleOrder * scaleOrder
 
-    int width = pow(2, scaleOrder+1);
-    vector<vector<char>> vec;
-    vec.resize(width, vector<char>(width));  // setting size of matrix to scaleOrder * scaleOrder
-    
-    vector<vector<char>> baseTriangle = generateSierpinskyTriangle(scaleOrder-1);
-    // Arrange baseTriangle into superTriangle with this arrangement
-    //
-    // * _
-    // * *
+        vector<vector<char>> miniMatrix = generateSierpinskyTriangle(scaleOrder - 1);
 
-    vector<vector<char>> emptyVec;
-    
+        // Creating megamatrix with this composition: |* 0|
+        //                                            |* *|
+        writeSubmatrixIntoMatrix(megaMatrix, miniMatrix, 0, 0);
+        writeSubmatrixIntoMatrix(megaMatrix, miniMatrix, width / 2, 0);
+        writeSubmatrixIntoMatrix(megaMatrix, miniMatrix, width / 2, width / 2);
+        return megaMatrix;
+    }
 }
 
-void printCharMatrix(const vector<vector<char>>& charMatrix) {
-    for (vector<char> vec : charMatrix) {
-        for (char c : vec) {
+void printCharMatrix(const vector<vector<char>> &charMatrix)
+{
+    for (vector<char> vec : charMatrix)
+    {
+        for (char c : vec)
+        {
             cout << c;
         }
         cout << endl;
     }
 }
 
-int main() {
+int main()
+{
     int n;
-    
     cout << "How large of a triangle you want?" << endl;
     cin >> n;
     cout << endl;
 
-    vector<vector<char>> result = {{'1', '1', '1'}, 
-                                   {'1', '1', '1'}, 
-                                   {'1', '1', '1'}};
-    vector<vector<char>> triangle = {{'*', ' '},
-                                     {'*', '*'}};
-    writeSubmatrixIntoMatrix(result, triangle, 1, 1);
-
-    printCharMatrix(result);
+    vector<vector<char>> sierpinsky = generateSierpinskyTriangle(n);
+    printCharMatrix(sierpinsky);
 }
